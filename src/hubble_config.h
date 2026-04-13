f7fc1f31885941ecffd68483bf76945245ef229c
Paul Buckley <paul@hubble.com>
2026-04-13 10:30:36 -0700

    sdk: update hubble-sdk to main and adapt app to breaking changes
    The vendored SDK moved from v1.0.0 to main, which renamed the build
    integration variables and crypto port macros. This also introduces a
    project-local SDK config so the app controls its own settings
    independently of the SDK defaults, and switches to device uptime
    counter mode.
    
    Signed-off-by: Paul Buckley <paul@hubble.com>

---
+++ src/hubble_config.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Hubble Network, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SRC_HUBBLE_CONFIG_H
+#define SRC_HUBBLE_CONFIG_H
+
+#define CONFIG_HUBBLE_BLE_NETWORK             1
+/* CONFIG_HUBBLE_SAT_NETWORK intentionally left undefined (disabled) */
+
+#define CONFIG_HUBBLE_KEY_SIZE                16
+
+#define CONFIG_HUBBLE_COUNTER_SOURCE_DEVICE_UPTIME 1
+#define CONFIG_HUBBLE_EID_ROTATION_PERIOD_SEC 86400
+
+#endif /* SRC_HUBBLE_CONFIG_H */
